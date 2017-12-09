28b14f1d2f04de3b8cbc3ec10f109b2398f4105d
yinzhiyizhi <yinzhiyizhi@gmail.com>
2017-12-09 09:06:47 +0800

    完成练习

--- 函数/递归函数.py
+++ 函数/递归函数.py
@@ -96,7 +96,12 @@ print(fact(5))
 # move(3, 'A', 'B', 'C')
 
 
-def move(n,a='A',b='B',c='C'):
+def move(n,a,b,c):
     if n==1:
         print(a,'-->',c)
-    
+    else:
+        move(n-1,a,c,b)
+        move(1,a,b,c)
+        move(n-1,b,a,c)
+
+move(3,'A','B','C')
