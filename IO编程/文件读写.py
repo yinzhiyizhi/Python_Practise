ec736bcccf3a7d88ba6f877e06c4d5a51ec34253
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-01-12 21:51:39 +0800

    学习文件读写

--- IO编程/文件读写.py
+++ IO编程/文件读写.py
@@ -40,17 +40,17 @@ f.close()
 # 一旦出错，后面的f.close()就不会调用。
 # 所以，为了保证无论是否出错都能正确地关闭文件，
 # 我们可以使用try ... finally来实现：
-try:
-    f=open('/path/to/file','r')
-    print(f.read())
-finally:
-    if f:
-        f.close()
+# try:
+#     f=open('/path/to/file','r')
+#     print(f.read())
+# finally:
+#     if f:
+#         f.close()
 
 # 但是每次都这么写实在太繁琐，
 # 所以，Python引入了with语句来自动帮我们调用close()方法：
-with open('/path/to/file','r')as f:
-    print(f.read())
+# with open('/path/to/file','r') as f:
+#     print(f.read())
 
 # 这和前面的try ... finally是一样的，但是代码更佳简洁，
 # 并且不必调用f.close()方法。
@@ -65,8 +65,8 @@ with open('/path/to/file','r')as f:
 # 如果文件很小，read()一次性读取最方便；
 # 如果不能确定文件大小，反复调用read(size)比较保险；
 # 如果是配置文件，调用readlines()最方便：
-for line in f.readlines():
-    pritn(line.strip()) # 把末尾的'\n'删掉
+# for line in f.readlines():
+#     pritn(line.strip()) # 把末尾的'\n'删掉
 
 
 # file-like Object
@@ -86,9 +86,68 @@ for line in f.readlines():
 # b'\xff\xd8\xff\xe1\x00\x18Exif\x00\x00...' # 十六进制表示的字节
 
 
+# 字符编码
+# 要读取非UTF-8编码的文本文件，需要给open()函数传入encoding参数，
+# 例如，读取GBK编码的文件：
+
+# >>> f = open('/Users/michael/gbk.txt', 'r', encoding='gbk')
+# >>> f.read()
+# '测试'
+
+# 遇到有些编码不规范的文件，你可能会遇到UnicodeDecodeError，
+# 因为在文本文件中可能夹杂了一些非法编码的字符。
+# 遇到这种情况，open()函数还接收一个errors参数，
+# 表示如果遇到编码错误后如何处理。最简单的方式是直接忽略：
+
+# >>> f = open('/Users/michael/gbk.txt', 'r', encoding='gbk', errors='ignore')
+
+
+# 写文件
+# 写文件和读文件是一样的，唯一区别是调用open()函数时，
+# 传入标识符'w'或者'wb'表示写文本文件或写二进制文件：
+
+# >>> f = open('/Users/michael/test.txt', 'w')
+# >>> f.write('Hello, world!')
+# >>> f.close()
+
+# 你可以反复调用write()来写入文件，但是务必要调用f.close()来关闭文件。
+# 当我们写文件时，操作系统往往不会立刻把数据写入磁盘，
+# 而是放到内存缓存起来，空闲的时候再慢慢写入。
+# 只有调用close()方法时，操作系统才保证把没有写入的数据全部写入磁盘。
+# 忘记调用close()的后果是数据可能只写了一部分到磁盘，剩下的丢失了。
+# 所以，还是用with语句来得保险：
+
+# with open('/Users/michael/test.txt', 'w') as f:
+#     f.write('Hello, world!')
+
+# 要写入特定编码的文本文件，请给open()函数传入encoding参数，
+# 将字符串自动转换成指定编码。
+
+# 细心的童鞋会发现，以'w'模式写入文件时，如果文件已存在，会直接覆盖（相当于删掉后新写入一个文件）。如果我们希望追加到文件末尾怎么办？可以传入'a'以追加（append）模式写入。
+
+# 所有模式的定义及含义可以参考Python的官方文档。
 
 
 
+# 小结
+# 在Python中，文件读写是通过open()函数打开的文件对象完成的。
+# 使用with语句操作文件IO是个好习惯。
 
 
 
+# 练习
+# 请将本地一个文本文件读为一个str并打印出来：
+# fpath = r'C:\Windows\system.ini'
+#
+# with open(fpath, 'r') as f:
+#     s = f.read()
+#     print(s)
+# 运行代码观察结果
+
+
+
+fpath = r'C:\Users\Administrator\Desktop\test.txt'
+
+with open(fpath, 'r') as f:
+    s = f.read()
+    print(s)
