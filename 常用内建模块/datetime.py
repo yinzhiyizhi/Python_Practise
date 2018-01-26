2eb54ac61a9ced66d2ac6b972f43ceb0f4687d0c
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-01-26 22:23:51 +0800

    学习datetime

--- 常用内建模块/datetime.py
+++ 常用内建模块/datetime.py
@@ -61,12 +61,111 @@ dt.timestamp() # 把datetime转换为timestamp
 
 
 
+# timestamp转换为datetime
+# 要把timestamp转换为datetime，使用datetime提供的fromtimestamp()方法：
+from datetime import datetime
+t=1515821640.0
+print(datetime.fromtimestamp(t))
+# 2018-01-13 13:34:00
+
+# 注意到timestamp是一个浮点数，它没有时区的概念，而datetime是有时区的。
+# 上述转换是在timestamp和本地时间做转换。
+
+# 本地时间是指当前操作系统设定的时区。例如北京时区是东8区，则本地时间：
+# 2018-01-13 13:34:00
+
+# 实际上就是UTC+8:00时区的时间：
+# 2018-01-13 13:34:00 UTC+8:00
+
+# 而此刻的格林威治标准时间与北京时间差了8小时，也就是UTC+0:00时区的时间应该是：
+# 2018-01-13 05:34:00 UTC+0:00
+
+# timestamp也可以直接被转换到UTC标准时区的时间：
+from datetime import datetime
+t=1515821640.0
+print(datetime.fromtimestamp(t)) # 本地时间
+# 2018-01-13 13:34:00
+print(datetime.utcfromtimestamp(t)) # UTC时间
+# 2018-01-13 05:34:00
 
 
 
+# str转换为datetime
+# 很多时候，用户输入的日期和时间是字符串，要处理日期和时间，
+# 首先必须把str转换为datetime。
+# 转换方法是通过datetime.strptime()实现，需要一个日期和时间的格式化字符串：
+from datetime import datetime
+cday=datetime.strptime('2015-6-1 18:19:59','%Y-%m-%d %H:%M:%S')
+print(cday)
+# 2015-06-01 18:19:59
+
+# 字符串'%Y-%m-%d %H:%M:%S'规定了日期和时间部分的格式。
+# 详细的说明请参考Python文档(https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior)。
+
+# 注意转换后的datetime是没有时区信息的。
 
 
 
+# datetime转换为str
+# 如果已经有了datetime对象，要把它格式化为字符串显示给用户，
+# 就需要转换为str，转换方法是通过strftime()实现的，
+# 同样需要一个日期和时间的格式化字符串：
+from datetime import datetime
+now=datetime.now()
+print(now.strftime('%a, %b %d %H:%M'))
+# Mon, May 05 16:28
 
 
+
+# datetime加减
+# 对日期和时间进行加减实际上就是把datetime往后或往前计算，得到新的datetime。
+# 加减可以直接用+和-运算符，不过需要导入timedelta这个类：
+from datetime import datetime,timedelta
+now=datetime.now()
+print(now)
+# 2018-01-26 21:10:22.018233
+print(now+timedelta(hours=10))
+# 2018-01-27 07:10:22.018233
+print(now-timedelta(days=1))
+# 2018-01-25 21:10:22.018233
+print(now+timedelta(days=2,hours=12))
+# 2018-01-29 09:10:22.018233
+
+# 可见，使用timedelta你可以很容易地算出前几天和后几天的时刻。
+
+
+
+# 本地时间转换为UTC时间
+# 本地时间是指系统设定时区的时间，
+# 例如北京时间是UTC+8:00时区的时间，而UTC时间指UTC+0:00时区的时间。
+
+# 一个datetime类型有一个时区属性tzinfo，
+# 但是默认为None，所以无法区分这个datetime到底是哪个时区，
+# 除非强行给datetime设置一个时区：
+from datetime import datetime,timedelta,timezone
+tz_utc_8=timezone(timedelta(hours=8)) # 创建时区UTC+8:00
+now=datetime.now()
+print(now)
+# 2018-01-26 21:31:30.841606
+dt=now.replace(tzinfo=tz_utc_8) # 强制设置为UTC+8:00
+print(dt)
+# 2018-01-26 21:31:30.841606+08:00
+
+# 如果系统时区恰好是UTC+8:00，那么上述代码就是正确的，否则，不能强制设置为UTC+8:00时区。
+
+
+
+# 时区转换
+# 我们可以先通过utcnow()拿到当前的UTC时间，再转换为任意时区的时间：
+# 拿到UTC时间，并强制设置时区为UTC+0:00:
+utc_dt=datetime.utcnow().replace(tzinfo=timezone.utc)
+print(utc_dt)
+
+
+# astimezone()将转换时区为北京时间:
+bj_dt=utc_dt.astimezone(timezone(timedelta(hours=8)))
+print(bj_dt)
+
+
+# astimezone()将转换时区为东京时间:
 
