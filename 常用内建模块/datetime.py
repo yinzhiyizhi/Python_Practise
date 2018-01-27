b98c9ff967aa5deec7f501a295e065cc43afe289
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-01-27 22:29:50 +0800

    学习datetime

--- 常用内建模块/datetime.py
+++ 常用内建模块/datetime.py
@@ -160,12 +160,72 @@ print(dt)
 # 拿到UTC时间，并强制设置时区为UTC+0:00:
 utc_dt=datetime.utcnow().replace(tzinfo=timezone.utc)
 print(utc_dt)
-
+# 2018-01-27 13:53:04.116667+00:00
 
 # astimezone()将转换时区为北京时间:
 bj_dt=utc_dt.astimezone(timezone(timedelta(hours=8)))
 print(bj_dt)
-
+# 2018-01-27 21:53:04.116667+08:00
 
 # astimezone()将转换时区为东京时间:
+tokyo_dt=utc_dt.astimezone(timezone(timedelta(hours=9)))
+print(tokyo_dt)
+# 2018-01-27 22:53:04.116667+09:00
+
+# astimezone()将bj_dt转换时区为东京时间:
+tokyo_dt2=bj_dt.astimezone(timezone(timedelta(hours=9)))
+print(tokyo_dt2)
+# 2018-01-27 22:53:04.116667+09:00
+
+# 时区转换的关键在于，拿到一个datetime时，
+# 要获知其正确的时区，然后强制设置时区，作为基准时间。
+
+# 利用带时区的datetime，通过astimezone()方法，可以转换到任意时区。
+
+# 注：不是必须从UTC+0:00时区转换到其他时区，
+# 任何带时区的datetime都可以正确转换，例如上述bj_dt到tokyo_dt的转换。
+
+
+
+# 小结
+# datetime表示的时间需要时区信息才能确定一个特定的时间，否则只能视为本地时间。
+
+# 如果要存储datetime，最佳方法是将其转换为timestamp再存储，
+# 因为timestamp的值与时区完全无关。
+
+
+
+# 练习
+# 假设你获取了用户输入的日期和时间如2015-1-21 9:01:30，
+# 以及一个时区信息如UTC+5:00，均是str，请编写一个函数将其转换为timestamp：
+# import re
+# from datetime import datetime, timezone, timedelta
+# def to_timestamp(dt_str, tz_str):
+#     pass
+# # 测试:
+# t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
+# assert t1 == 1433121030.0, t1
+#
+# t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
+# assert t2 == 1433121030.0, t2
+#
+# print('ok')
+
+
+
+import re
+from datetime import datetime, timezone, timedelta
+def to_timestamp(dt_str, tz_str):
+
+
+
+# 测试:
+t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
+assert t1 == 1433121030.0, t1
+
+t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
+assert t2 == 1433121030.0, t2
+
+print('ok')
+
 
