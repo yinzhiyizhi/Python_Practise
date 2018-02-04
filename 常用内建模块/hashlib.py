c0ba97f055be40d3f4cd9afef84ce4a0a9e8170d
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-02-04 21:48:05 +0800

    、、、

--- 常用内建模块/hashlib.py
+++ 常用内建模块/hashlib.py
@@ -44,13 +44,29 @@ md5=hashlib.md5()
 md5.update('How to use md5 in python hashlib?'.encode('utf-8'))
 print(md5.hexdigest())
 # 计算结果如下：
+# 7fcb9cb760463e9a8623aaac99f9dae1
 
-
-# MD5是最常见的摘要算法，速度很快，生成结果是固定的128 bit字节，通常用一个32位的16进制字符串表示。
+# MD5是最常见的摘要算法，速度很快，生成结果是固定的128 bit字节，
+# 通常用一个32位的16进制字符串表示。
 
 # 另一种常见的摘要算法是SHA1，调用SHA1和调用MD5完全类似：
+import hashlib
 
+sha1=hashlib.sha1()
+sha1.update('how to use sha1 in '.encode('utf-8'))
+sha1.update('python hashlib?'.encode('utf-8'))
+print(sha1.hexdigest())
 
+# SHA1的结果是160 bit字节，通常用一个40位的16进制字符串表示。
+
+# 比SHA1更安全的算法是SHA256和SHA512，不过越安全的算法不仅越慢，而且摘要长度更长。
+
+# 有没有可能两个不同的数据通过某个摘要算法得到了相同的摘要？
+# 完全有可能，因为任何摘要算法都是把无限多的数据集合映射到一个有限的集合中。
+# 这种情况称为碰撞，
+# 比如Bob试图根据你的摘要反推出一篇文章'how to learn hashlib in python - by Bob'，
+# 并且这篇文章的摘要恰好和你的文章完全一致，这种情况也并非不可能出现，
+# 但是非常非常困难。
 
 
 
