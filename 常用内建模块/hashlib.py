2224df959d130ea7204d35cf05f234929fe41b1d
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-02-05 21:33:04 +0800

    ...

--- 常用内建模块/hashlib.py
+++ 常用内建模块/hashlib.py
@@ -70,8 +70,126 @@ print(sha1.hexdigest())
 
 
 
+# 摘要算法应用
+# 摘要算法能应用到什么地方？举个常用例子：
+
+# 任何允许用户登录的网站都会存储用户登录的用户名和口令。
+# 如何存储用户名和口令呢？方法是存到数据库表中：
+
+# name	    password
+# michael	123456
+# bob	    abc999
+# alice	    alice2008
+
+# 如果以明文保存用户口令，如果数据库泄露，所有用户的口令就落入黑客的手里。
+# 此外，网站运维人员是可以访问数据库的，也就是能获取到所有用户的口令。
+
+# 正确的保存口令的方式是不存储用户的明文口令，而是存储用户口令的摘要，比如MD5：
+
+# username	password
+# michael	e10adc3949ba59abbe56e057f20f883e
+# bob	    878ef96e86145580c38c87f0410ad153
+# alice	    99b1c2188db85afee403b1536010c2c9
+
+# 当用户登录时，首先计算用户输入的明文口令的MD5，
+# 然后和数据库存储的MD5对比，如果一致，说明口令输入正确，如果不一致，口令肯定错误。
 
 
+
+# 小结
+# 摘要算法在很多地方都有广泛的应用。
+# 要注意摘要算法不是加密算法，不能用于加密（因为无法通过摘要反推明文），
+# 只能用于防篡改，但是它的单向计算特性决定了可以在不存储明文口令的情况下验证用户口令。
+
+
+
+# 练习
+# 根据用户输入的口令，计算出存储在数据库中的MD5口令：
+
+# def calc_md5(password):
+#     pass
+
+# 存储MD5的好处是即使运维人员能访问数据库，也无法获知用户的明文口令。
+
+# 设计一个验证用户登录的函数，根据用户输入的口令是否正确，返回True或False：
+
+# db = {
+#     'michael': 'e10adc3949ba59abbe56e057f20f883e',
+#     'bob': '878ef96e86145580c38c87f0410ad153',
+#     'alice': '99b1c2188db85afee403b1536010c2c9'
+# }
+# def login(user, password):
+#     pass
+
+# # 测试:
+# assert login('michael', '123456')
+# assert login('bob', 'abc999')
+# assert login('alice', 'alice2008')
+# assert not login('michael', '1234567')
+# assert not login('bob', '123456')
+# assert not login('alice', 'Alice2008')
+# print('ok')
+
+
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+def login(user, password):
+    return calc_md5(password)==db[user]
+
+def calc_md5(password):
+    md5=hashlib.md5()
+    md5.update(password.encode('utf-8'))
+    return md5.hexdigest()
+
+
+
+# 测试:
+assert login('michael', '123456')
+assert login('bob', 'abc999')
+assert login('alice', 'alice2008')
+assert not login('michael', '1234567')
+assert not login('bob', '123456')
+assert not login('alice', 'Alice2008')
+print('ok')
+
+
+
+# 采用MD5存储口令是否就一定安全呢？也不一定。
+# 假设你是一个黑客，已经拿到了存储MD5口令的数据库，
+# 如何通过MD5反推用户的明文口令呢？
+# 暴力破解费事费力，真正的黑客不会这么干。
+
+# 考虑这么个情况，很多用户喜欢用123456，888888，password这些简单的口令，
+# 于是，黑客可以事先计算出这些常用口令的MD5值，得到一个反推表：
+
+# 'e10adc3949ba59abbe56e057f20f883e': '123456'
+# '21218cca77804d2ba1922c33e0151105': '888888'
+# '5f4dcc3b5aa765d61d8327deb882cf99': 'password'
+
+# 这样，无需破解，只需要对比数据库的MD5，黑客就获得了使用常用口令的用户账号。
+
+# 对于用户来讲，当然不要使用过于简单的口令。
+# 但是，我们能否在程序设计上对简单口令加强保护呢？
+
+# 由于常用口令的MD5值很容易被计算出来，
+# 所以，要确保存储的用户口令不是那些已经被计算出来的常用口令的MD5，
+# 这一方法通过对原始口令加一个复杂字符串来实现，俗称“加盐”：
+
+# def calc_md5(password):
+#     return get_md5(password + 'the-Salt')
+
+# 经过Salt处理的MD5口令，只要Salt不被黑客知道，
+# 即使用户输入简单口令，也很难通过MD5反推明文口令。
+
+# 但是如果有两个用户都使用了相同的简单口令比如123456，
+# 在数据库中，将存储两条相同的MD5值，这说明这两个用户的口令是一样的。
+# 有没有办法让使用相同口令的用户存储不同的MD5呢？
+
+# 如果假定用户无法修改登录名，就可以通过把登录名作为Salt的一部分来计算MD5，
+# 从而实现相同口令的用户也存储不同的MD5。
 
 
 
