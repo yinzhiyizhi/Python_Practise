d5d4adac265b6a03c621dfa602a6c231695251b0
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-02-09 22:28:36 +0800

    itertools

--- 常用内建模块/itertools.py
+++ 常用内建模块/itertools.py
@@ -7,7 +7,7 @@
 
 import itertools
 natuals=itertools.count(1)
-for n in natuals:
+# for n in natuals:
     # print(n)
 # '''
 # 1
@@ -22,7 +22,7 @@ for n in natuals:
 
 import itertools
 cs=itertools.cycle('ABC') # 注意字符串也是序列的一种
-for c in cs:
+# for c in cs:
     # print(c)
 # '''
 # 'A'
@@ -36,6 +36,7 @@ for c in cs:
 # 同样停不下来。
 
 # repeat()负责把一个元素无限重复下去，不过如果提供第二个参数就可以限定重复次数：
+
 ns=itertools.repeat('A',3)
 for n in ns:
     print(n)
@@ -57,6 +58,70 @@ list(ns)
 
 # itertools提供的几个迭代器操作函数更加有用：
 
+
+# chain()
+# chain()可以把一组迭代对象串联起来，形成一个更大的迭代器：
+
+for c in itertools.chain('ABC','XYZ'):
+    print(c)
+# 迭代效果：'A' 'B' 'C' 'X' 'Y' 'Z'
+
+
+# groupby()
+# groupby()把迭代器中相邻的重复元素挑出来放在一起：
+
+for key,group in itertools.groupby('AAABBBCCAAA'):
+    print(key,list(group))
+# A ['A', 'A', 'A']
+# B ['B', 'B', 'B']
+# C ['C', 'C']
+# A ['A', 'A', 'A']
+
+# 实际上挑选规则是通过函数完成的，只要作用于函数的两个元素返回的值相等，
+# 这两个元素就被认为是在一组的，而函数返回值作为组的key。
+# 如果我们要忽略大小写分组，就可以让元素'A'和'a'都返回相同的key：
+
+for key,group in itertools.groupby('AaaBBbcCAAa',lambda c:c.upper()):
+    print(key,list(group))
+# A ['A', 'a', 'a']
+# B ['B', 'B', 'b']
+# C ['c', 'C']
+# A ['A', 'A', 'a']
+
+
+
+# 小结
+# itertools模块提供的全部是处理迭代功能的函数，
+# 它们的返回值不是list，而是Iterator，只有用for循环迭代的时候才真正计算。
+
+
+
+# 练习
+# 计算圆周率可以根据公式：
+
+# 利用Python提供的itertools模块，我们来计算这个序列的前N项和：
+
+import itertools
+def pi(N):
+    ' 计算pi的值 '
+    # step 1: 创建一个奇数序列: 1, 3, 5, 7, 9, ...
+
+    # step 2: 取该序列的前N项: 1, 3, 5, 7, 9, ..., 2*N-1.
+
+    # step 3: 添加正负符号并用4除: 4/1, -4/3, 4/5, -4/7, 4/9, ...
+
+    # step 4: 求和:
+    return 3.14
+# 测试:
+print(pi(10))
+print(pi(100))
+print(pi(1000))
+print(pi(10000))
+assert 3.04 < pi(10) < 3.05
+assert 3.13 < pi(100) < 3.14
+assert 3.140 < pi(1000) < 3.141
+assert 3.1414 < pi(10000) < 3.1415
+print('ok')
 
 
 
