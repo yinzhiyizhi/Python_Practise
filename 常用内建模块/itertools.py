7462ab181cbde24a86b82100eb71c37cd8c912ab
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-02-10 22:04:45 +0800

    itertools

--- 常用内建模块/itertools.py
+++ 常用内建模块/itertools.py
@@ -101,17 +101,47 @@ for key,group in itertools.groupby('AaaBBbcCAAa',lambda c:c.upper()):
 
 # 利用Python提供的itertools模块，我们来计算这个序列的前N项和：
 
+# import itertools
+# def pi(N):
+#     ' 计算pi的值 '
+#     # step 1: 创建一个奇数序列: 1, 3, 5, 7, 9, ...
+
+#     # step 2: 取该序列的前N项: 1, 3, 5, 7, 9, ..., 2*N-1.
+
+#     # step 3: 添加正负符号并用4除: 4/1, -4/3, 4/5, -4/7, 4/9, ...
+
+#     # step 4: 求和:
+#     return 3.14
+# # 测试:
+# print(pi(10))
+# print(pi(100))
+# print(pi(1000))
+# print(pi(10000))
+# assert 3.04 < pi(10) < 3.05
+# assert 3.13 < pi(100) < 3.14
+# assert 3.140 < pi(1000) < 3.141
+# assert 3.1414 < pi(10000) < 3.1415
+# print('ok')
+
+
+
+
 import itertools
 def pi(N):
     ' 计算pi的值 '
     # step 1: 创建一个奇数序列: 1, 3, 5, 7, 9, ...
+    xu=itertools.count(1,2)
 
     # step 2: 取该序列的前N项: 1, 3, 5, 7, 9, ..., 2*N-1.
+    ns=list(itertools.takewhile(lambda y:y<=2*N-1,xu))
 
     # step 3: 添加正负符号并用4除: 4/1, -4/3, 4/5, -4/7, 4/9, ...
+    nc=itertools.cycle([4,-4])
+
+    result=[next(nc)/i for i in ns]
 
     # step 4: 求和:
-    return 3.14
+    return sum(result)
 # 测试:
 print(pi(10))
 print(pi(100))
@@ -122,7 +152,4 @@ assert 3.13 < pi(100) < 3.14
 assert 3.140 < pi(1000) < 3.141
 assert 3.1414 < pi(10000) < 3.1415
 print('ok')
-
-
-
 
