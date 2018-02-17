f16516c4abead4a2361011b086edd27105b212c0
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-02-17 21:48:25 +0800

    ...

--- 常用内建模块/urllib.py
+++ 常用内建模块/urllib.py
@@ -182,7 +182,26 @@ with opener.open('http://www.example.com/login.html') as f:
 
 
 
+# 练习
+# 利用urllib读取JSON，然后将JSON解析为Python对象：
+# from urllib import request
+# def fetch_data(url):
+#     return ''
+# # 测试
+# URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+# data = fetch_data(URL)
+# print(data)
+# assert data['query']['results']['channel']['location']['city'] == 'Beijing'
+# print('ok')
 
 
 
-
+from urllib import request
+def fetch_data(url):
+    return ''
+# 测试
+URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+data = fetch_data(URL)
+print(data)
+assert data['query']['results']['channel']['location']['city'] == 'Beijing'
+print('ok')
