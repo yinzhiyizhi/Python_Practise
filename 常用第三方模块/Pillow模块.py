d3fa5d93fd44f7a7465246f5d61fd3f7a2485ee5
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-02-28 21:46:36 +0800

    文件名和内置库有冲突

---
+++ 常用第三方模块/Pillow模块.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# PIL：Python Imaging Library，已经是Python平台事实上的图像处理标准库了。
+# PIL功能非常强大，但API却非常简单易用。
+
+# 由于PIL仅支持到Python 2.7，加上年久失修，
+# 于是一群志愿者在PIL的基础上创建了兼容的版本，名字叫Pillow，
+# 支持最新Python 3.x，又加入了许多新特性，
+# 因此，我们可以直接安装使用Pillow。
+
+# 安装Pillow
+# 如果安装了Anaconda，Pillow就已经可用了。
+# 否则，需要在命令行下通过pip安装：
+
+# $ pip install pillow
+
+# 如果遇到Permission denied安装失败，请加上sudo重试。
+
+
+
+# 操作图像
+# 来看看最常见的图像缩放操作，只需三四行代码：
+
+from PIL import Image
+
+# 打开一个jpg图像文件，注意是当前路径：
+im=Image.open('test.jpg')
+# 获得图像尺寸：
+
+
