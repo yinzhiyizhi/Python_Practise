12bbf7e26941f02a462d56b5c7b8e78597cf2f3f
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-03-07 21:38:28 +0800

    requests

--- 常用第三方模块/requests模块.py
+++ 常用第三方模块/requests模块.py
@@ -33,5 +33,50 @@ r.text
 
 r=requests.get('https://www.douban.com/search',params={'q':'python','cat':'1001'})
 
-# r.url # 实际请求的URL
+r.url # 实际请求的URL
 # 'https://www.douban.com/search?q=python&cat=1001'
+
+# requests自动检测编码，可以使用encoding属性查看：
+
+r.encoding
+# 'utf-8'
+
+# 无论响应是文本还是二进制内容，我们都可以用content属性获得bytes对象：
+
+r.content
+# b'<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n...'
+
+# requests的方便之处还在于，对于特定类型的响应，例如JSON，可以直接获取：
+
+r=requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json')
+r.json()
+# {'query': {'count': 1, 'created': '2017-11-17T07:14:12Z', ...
+
+# 需要传入HTTP Header时，我们传入一个dict作为headers参数：
+
+r=requests.get('https://www.douban.com/',headers={'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit'})
+r.text
+# '<!DOCTYPE html>\n<html>\n<head>\n<meta charset="UTF-8">\n <title>豆瓣(手机版)</title>...'
+
+# 要发送POST请求，只需要把get()方法变成post()，然后传入data参数作为POST请求的数据：
+r=requests.post('https://accounts.douban.com/login',data={'from_email':'abc@example.com','form_password':'123456'})
+
+# requests默认使用application/x-www-form-urlencoded对POST数据编码。
+# 如果要传递JSON数据，可以直接传入json参数：
+
+params={'key':'value'}
+# r=requests.post(url,json=params) # 内部自动序列号为JSON
+
+# 类似的，上传文件需要更复杂的编码格式，但是requests把它简化成files参数：
+
+upload_files={'file':open('report.xls','rb')}
+# r=requests.post(url,files=upload_files)
+
+# 在读取文件时，注意务必使用'rb'即二进制模式读取，这样获取的bytes长度才是文件的长度。
+
+# 把post()方法替换为put()，delete()等，就可以以PUT或DELETE方式请求资源。
+
+# 除了能轻松获取响应内容外，requests对获取HTTP响应的其他信息也非常简单。例如，获取响应头：
+
+
+
