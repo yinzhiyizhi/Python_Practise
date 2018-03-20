2626bbd938eb4212bd33c17c568b73398fb3217c
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-03-20 09:19:19 +0800

    tcp

--- 网络编程/TCP编程.py
+++ 网络编程/TCP编程.py
@@ -49,3 +49,50 @@ s.connect(('www.sina.com.cn',80))
 # s.connect(('www.sina.com.cn',80))
 
 # 注意参数是一个tuple，包含地址和端口号。
+
+# 建立TCP连接后，我们就可以向新浪服务器发送请求，要求返回首页的内容：
+
+# 发送数据：
+s.send(b'GET / HTTP/1.1\r\nHost:www.sina.com.cn\r\nConnection:close\r\n\r\n')
+
+# TCP连接创建的是双向通道，双方都可以同时给对方发数据。
+# 但是谁先发谁后发，怎么协调，要根据具体的协议来决定。
+# 例如，HTTP协议规定客户端必须先发请求给服务器，
+# 服务器收到后才发数据给客户端。
+
+# 发送的文本格式必须符合HTTP标准，
+# 如果格式没问题，接下来就可以接收新浪服务器返回的数据了：
+
+# 接收数据：
+buffer=[]
+while True:
+    # 每次最多接受1k字节：
+    d=s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data=b''.join(buffer)
+
+# 接收数据时，调用recv(max)方法，一次最多接收指定的字节数，
+# 因此，在一个while循环中反复接收，
+# 直到recv()返回空数据，表示接收完毕，退出循环。
+
+# 当我们接收完数据后，调用close()方法关闭Socket，
+# 这样，一次完整的网络通信就结束了：
+
+# 关闭连接：
+s.close()
+
+# 接收到的数据包括HTTP头和网页本身，
+# 我们只需要把HTTP头和网页分离一下，把HTTP头打印出来，网页内容保存到文件：
+
+header,html=data.split(b'\r\n\r\n',1)
+print(header.decode('utf-8'))
+# 把接受的数据写入文件：
+with open('sina.html','wb') as f:
+    f.write(html)
+
+# 现在，只需要在浏览器中打开这个sina.html文件，就可以看到新浪的首页了。
+
+        
