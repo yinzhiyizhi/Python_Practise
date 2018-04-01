6e1002cf333d8f2dbeded7bee73287885b07d5d3
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-04-01 21:28:14 +0800

    smtp

--- 电子邮件/SMTP发送邮件.py
+++ 电子邮件/SMTP发送邮件.py
@@ -17,3 +17,25 @@ msg=MIMEText('hello, send by Python...','plain','utf-8')
 # 传入'plain'表示纯文本，最终的MIME就是'text/plain'，
 # 最后一定要用utf-8编码保证多语言兼容性。
 
+# 然后，通过SMTP发出去：
+
+# 输入Email地址和口令:
+from_addr = input('From: ')
+password = input('Password: ')
+# 输入收件人地址:
+to_addr = input('To: ')
+# 输入SMTP服务器地址:
+smtp_server = input('SMTP server: ')
+
+import smtplib
+server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
+
+# 我们用set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息。
+# SMTP协议就是简单的文本命令和响应。
+# login()方法用来登录SMTP服务器，sendmail()方法就是发邮件，
+# 由于可以一次发给多个人，所以传入一个list，
+# 邮件正文是一个str，as_string()把MIMEText对象变成str。
